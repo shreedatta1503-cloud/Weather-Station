340ee9e3af29da61f3dae5dcef5c65cb29a4f9af
shreedatta1503-cloud <shreedatta1503@gmail.com>
2026-04-30 16:04:41 +0530

    Weather header file

---
+++ libraries/AP_Arming/weather.h
@@ -0,0 +1,11 @@
+#pragma once
+
+extern float weather_temperature;
+extern float weather_humidity;
+extern float weather_altitude;
+extern uint8_t weather_status;
+extern uint32_t weather_last_update_ms;
+
+//#define WEATHER_SAFE 0
+//#define WEATHER_WARNING 1
+//#define WEATHER_DANGER 2
